c03f9d96c30e29933b0934258714defcd25bcf3f
unknown <home0@naver.com>
2020-06-10 10:03:17 +0900

    middleWare-revised

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; //리덕스 적용하기
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware';
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware)); //스토어의 생성
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
